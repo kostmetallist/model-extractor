96f5cf38ef052d04a7862d5c3878941653292882
kostmetallist <kostmetallist@gmail.com>
2019-04-15 02:34:09 +0300

    Model package content changes * renamed Model.java to TransSystem.java * TransSystem class extends JGraphT's DefaultDirectedWeightedGraph to represent     transition system * defined custom vertex and edge classes

---
+++ src/model/TransSystem.java
@@ -0,0 +1,71 @@
+package model;
+
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
+import java.util.List;
+import java.util.ArrayList;
+
+
+class TSVertex {
+	
+	private List<String> state = new ArrayList<>();
+	
+	
+	public TSVertex() {}
+		
+	// copy constructor
+	public TSVertex(List<String> state) {
+		
+		for (String each : state) {
+			this.state.add(each);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		
+		String result = "{";
+		for (String each : this.state) {
+			result += each + ", ";
+		}
+		
+		// if we have added at least one element
+		if (result.length() > 1) {
+			result = result.substring(0, result.length()-2);
+		}
+		
+		return result + "}";
+	}
+}
+
+
+class TSEdge extends DefaultWeightedEdge {
+	
+	private static final long serialVersionUID = 81275L;
+	private String label;
+	
+	public TSEdge(String label) {
+		this.label = label;
+	}
+	
+	public String getLabel() {
+		return this.label;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + this.getSource() + " : " + this.getTarget() + 
+        		" : " + label + ")";
+	}
+}
+
+
+public class TransSystem extends 
+	DefaultDirectedWeightedGraph<TSVertex, TSEdge> {
+	
+	private static final long serialVersionUID = 19756912L;
+	
+	public TransSystem() {
+		super(TSEdge.class);
+	}
+}
